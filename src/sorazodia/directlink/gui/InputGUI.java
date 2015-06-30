dc76a7d1f4d0422232e833a1d8e6bdcc53edef6d
SoraZodia <SoraZodia@users.noreply.github.com>
2015-06-29 22:45:05 -1000

    Added button, tweaked frame a bit

--- src/sorazodia/directlink/gui/InputGUI.java
+++ src/sorazodia/directlink/gui/InputGUI.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -31,6 +32,8 @@ public class InputGUI extends JPanel implements FocusListener {
   private JTextField input = new JTextField(100);
   /** Where the direct download is placed */
   private JTextField output = new JTextField(100);
+  /**Alternate way to get the direct link rather then using the Enter key*/
+  private JButton convertButton = new JButton(addSpace("Convert Link", 74));
   /** Holds the direct download URL */
   private String directLink = "";
 
@@ -39,9 +42,10 @@ public class InputGUI extends JPanel implements FocusListener {
    */
   protected InputGUI() {
     this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-    this.setPreferredSize(new Dimension(600, 80));
+    this.setPreferredSize(new Dimension(550, 100));
     this.writeLabel();
     this.prepareTextField();
+    this.prepareButton();
     this.prepareCompoundLayout();
   }
 
@@ -50,24 +54,35 @@ public class InputGUI extends JPanel implements FocusListener {
    * Readies the input and output line for user interaction
    */
   private void prepareTextField() {
-    input.addActionListener((ActionEvent action) -> {
-      if (input.getText().isEmpty())
-        return;
-
-      directLink = DirectLink.convertLink(input.getText());
-      if (!directLink.equalsIgnoreCase(DirectLink.INVALID))
-        DirectLink.addToClipboard(directLink);
-
-      output.setText(directLink);
-    });
+    input.addActionListener((ActionEvent action) -> toClipBoard());
     input.setMaximumSize(input.getPreferredSize());
     input.addFocusListener(this);
     output.setMaximumSize(output.getPreferredSize());
     output.addFocusListener(this);
     // output.setEditable(false);
   }
+  
+  /**
+   * Readies the button for user interaction
+   */
+  private void prepareButton() {
+    convertButton.addActionListener((ActionEvent action) -> toClipBoard());
+  }
+  
+  /**
+   * Convert and copy link into clipboard
+   */
+  private void toClipBoard() {
+    if (input.getText().isEmpty())
+      return;
 
-
+    directLink = DirectLink.convertLink(input.getText());
+    if (!directLink.equalsIgnoreCase(DirectLink.INVALID))
+      DirectLink.addToClipboard(directLink);
+    
+    output.setText(directLink);
+  }
+  
   /**
    * Adds the label to the label so the user can see the labels... I'll take my leave now
    */
@@ -83,11 +98,24 @@ public class InputGUI extends JPanel implements FocusListener {
   private void prepareCompoundLayout() {
     this.add(inputDia);
     this.add(input);
+    this.add(convertButton);
     this.add(outputDia);
     this.add(output);
   }
 
 
+  /**
+   * Add extra space to a string to make its button longer
+   */
+  private String addSpace(String str, int spaces) {
+    StringBuilder editor = new StringBuilder(str);
+    for (int x = 0; x < spaces; x++) {
+      editor.insert(0, " ");
+      editor.append(" ");
+    }
+    return editor.toString();
+  }
+  
   /**
    * Creates the GUI
    */
@@ -97,8 +125,8 @@ public class InputGUI extends JPanel implements FocusListener {
     JFrame window = new JFrame("Dropbox Link Convertor");
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.add(inputPanel);
-    window.setMinimumSize(new Dimension(600, 125));
     window.setIconImage(Toolkit.getDefaultToolkit().getImage(inputPanel.getClass().getResource("/resources/icon.png")));
+    window.setResizable(false);
     
     window.pack();
     window.setVisible(true);
